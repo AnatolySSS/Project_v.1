209195a3c2aea37e7a38b74cc67ab0a859334738
Unknown <Anatoly.Shilyaev@gmail.com>
2019-02-08 22:18:34 +0700

    Script

---
+++ script.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById("myCanvas"),
+    context = canvas.getContext("2d");
+
+var img = document.createElement("img");;
+    img.src =  "round1.png";
+    //img.src =  "http://www.playcast.ru/uploads/2017/04/06/22239012.png"; in case of nuclear war
+var xPosition = Math.random() * 330;
+var yPosition = Math.random() * 630;
+
+    img.onload = function() {
+    context.drawImage(img, xPosition, yPosition, 70, 70);
+    };
+
+    // xPosition = function randomPositionX () {
+    //               return Math.random() * 330;
+    //             }
+    //
+    // yPosition = function randomPositionY () {
+    //               return Math.random() * 630;
+    //             }
+    //
+    //
+    //   canvas.addEventListener('mousedown', function () {
+    //   xPosition = function randomPositionX () {
+    //                    return Math.random() * 330;
+    //                  }
+    //
+    //     yPosition = function randomPositionY () {
+    //                   return Math.random() * 630;
+    //                 }
+    //   });
